b59a85774c414ac2752c9f71170dde83eec5fe36
Noel Mandal <noelmandal@gmail.com>
2018-11-12 17:42:57 -0800

    made a kewl project

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 
-
-
 const apiKey = process.env.API_KEY;
 const yelpUrl = 'https://api.yelp.com/v3/businesses/search';
 const cors = 'https://cors-anywhere.herokuapp.com/';
 const ROOT_URL = cors + yelpUrl;
-console.log("apikey:", apiKey)
 export const FETCH_DATA = "FETCH_DATA";
 
 export function fetchData(input) {
