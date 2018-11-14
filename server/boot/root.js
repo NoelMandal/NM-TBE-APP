498982d02363b0568b7e71170530a79f7e0d8097
Noel Mandal <noelmandal@gmail.com>
2018-11-13 21:24:25 -0800

    set env to proxy ready for heroku

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,37 @@
-'use strict';
+"use strict";
+const axios = require("axios");
+require('dotenv').config();
 
-// module.exports = function(server) {
-//   // Install a `/` route that returns server status
-//   var router = server.loopback.Router();
-//   router.get('/', server.loopback.status());
-//   server.use(router);
-// };
+
+module.exports = function(server) {
+  // Install a `/` route that returns server status
+  var router = server.loopback.Router();
+  
+  //   router.get('/', server.loopback.status());
+  router.get("/proxy/yelp", (req, res) => {
+    const term = req.query
+    const apiKey = process.env.API_KEY;
+    const yelpUrl = "https://api.yelp.com/v3/businesses/search";
+    console.log('term is', term)
+    axios
+      .get(yelpUrl, {
+        headers: {
+          Authorization: "Bearer " + apiKey
+        },
+        params: {
+          term,
+          location: "92139"
+        }
+      })
+      .then(response => {
+        console.log(".then ", response
+        );
+        res.send(response.data); // <= send data to the client
+      })
+      .catch(err => {
+        console.log(err);
+        res.send({ err }); // <= send error
+      });
+  });
+  server.use(router);
+};
